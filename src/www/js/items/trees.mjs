5fbae49114019265a919cf042e396a7af0c7ad2f
slebetman <slebetman@gmail.com>
2024-10-24 00:42:27 +0800

    Remove test code

--- src/www/js/items/trees.mjs
+++ src/www/js/items/trees.mjs
@@ -7,16 +7,6 @@ export class Tree1 extends Sprite {
 		y: 275,
 	}
 
-	action () {
-		if (Math.random() > 0.9) {
-			const amount = (Math.random() - 0.5) / 4;
-
-			css(this.element,{
-				transform: `skew(${amount}deg) translate(${-amount*1.9}px)`
-			})
-		}
-	}
-
 	constructor (x, y) {
 		super('./images/tree1.png', 260, 320,{
 			x1: 120, x2: 160,
