6ab4ba79977b0f95b052837460a2047e0a24e4d9
slebetman <slebetman@gmail.com>
2024-10-31 00:01:31 +0800

    Removed unused code and images

--- src/www/js/items/trees.mjs
+++ src/www/js/items/trees.mjs
@@ -21,66 +21,12 @@ export class Tree1 extends Sprite {
 
 let idx = 0;
 
-const treesSprites = [
-	[0, 0], [-200, 0], [-400, 0],
-	[0, -300], [-200, -300], [-400, -300]
-]
-
-export class Trees extends Sprite {
-	static base = {
-		x: 100,
-		y: 260,
-	}
-
-	constructor (x, y) {
-		idx = (idx + 1) % treesSprites.length;
-		const [offsetX, offsetY] = treesSprites[idx];
-
-		super('./images/trees.png', 200, 300,{
-			x1: 20, x2: 180,
-			y1: 230, y2: 280,
-		}, offsetX, offsetY);
-		this.setXY(x,y);
-
-		// attachDebugBounds(this);
-	}
-}
-
-// const treesSprites2 = [
-// 	[0, 0], [-240, 0], [-240, -320], [-480, 0], [-480, -320],
-// 	[0, -320], [-240, -320], [-240, 0], [-480, -320], [-480, 0],
-// ]
-
-const treesSprites2 = [
-	[0, 0], [-240, 0], [-480, 0], [-240, 0], [-480, 0],
-]
-
-export class Trees2 extends Sprite {
-	static base = {
-		x: 120,
-		y: 275,
-	}
-
-	constructor (x, y) {
-		idx = (idx + 1) % treesSprites2.length;
-		const [offsetX, offsetY] = treesSprites2[idx];
-
-		super('./images/trees2.png', 240, 320,{
-			x1: 100, x2: 140,
-			y1: 265, y2: 290,
-		}, offsetX, offsetY);
-		this.setXY(x,y);
-
-		// attachDebugBounds(this);
-	}
-}
-
 const treesSprites3 = [
 	[0, 0], [-240, 0], [-240, -320], [-480, 0], [-480, -320],
 	[0, -320], [-240, -320], [-240, 0], [-480, -320], [-480, 0],
 ]
 
-export class Trees3 extends Sprite {
+export class Trees extends Sprite {
 	static base = {
 		x: 120,
 		y: 275,
