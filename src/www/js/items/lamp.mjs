0c8bf90bc7395259c2f9c9fdc3a5732eac690162
slebetman <slebetman@gmail.com>
2024-10-31 10:07:57 +0800

    Animage lamp being chopped

--- src/www/js/items/lamp.mjs
+++ src/www/js/items/lamp.mjs
@@ -13,10 +13,29 @@ export class Lamp extends Sprite {
 			x1: 30, x2: 70,
 			y1: 165, y2: 200,
 		});
+
+		this.state = 0;
+
 		this.setXY(x,y);
 	}
 
-	interact () {
-		dialog("It's just a street lamp.");
+	interact (actor) {
+		if (!actor.tool || actor.tool.constructor.name !== 'Axe') {
+			dialog("It's just a street lamp.");
+		}
+		else if (actor.tool.constructor.name === 'Axe') {
+			actor.interacting = true;
+			this.state = (this.state + 1) % 8;
+			css(this.element,{
+				transform: this.state < 4 ? 'skewX(0.5deg) translateX(-1px)' : '',
+			});
+			clearTimeout(this.timeout);
+			this.timeout = setTimeout(() => {
+				this.state = 0;
+				css(this.element,{
+					transform: ''
+				});
+			}, 500);
+		}
 	}
 }
